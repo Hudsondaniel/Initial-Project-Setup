2a9d59b896b0292d5dec8e9e27a5efb720ca7ecf
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-09-09 22:26:50 +0530

    Update dependency and webpack configuration details

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,25 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.js', 
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
+        filename: 'main.js', 
+        path: path.resolve(__dirname, 'dist'), 
+        publicPath: '/dist/' 
     },
+    mode: 'development',
     module: {
         rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                ],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.html$/,
-                loader: 'html-loader',
-            },
-        ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-        }),
-    ],
-    devtool: 'source-map',
-    mode: 'development',
+        {
+            test: /\.css$/, 
+            use: ['style-loader', 'css-loader'] 
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/, 
+            use: ['file-loader'] 
+        },
+    ]
+    
+    }
 };
